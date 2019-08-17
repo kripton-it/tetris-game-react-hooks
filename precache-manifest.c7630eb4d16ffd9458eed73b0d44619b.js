fd9434ecce5c702b73aaccfa5354b36f24197979
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-17 15:54:19 +0500

    Updates

---
+++ precache-manifest.c7630eb4d16ffd9458eed73b0d44619b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cb020a68a76321e6390cf2ae4a843b",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "ef2ad85e90961b8de27e",
+    "url": "/tetris/static/css/main.4f9925c9.chunk.css"
+  },
+  {
+    "revision": "7f08dd74e6ba5821f982",
+    "url": "/tetris/static/js/2.f942d414.chunk.js"
+  },
+  {
+    "revision": "ef2ad85e90961b8de27e",
+    "url": "/tetris/static/js/main.b1dd77b0.chunk.js"
+  },
+  {
+    "revision": "082e323fdaa036437dff",
+    "url": "/tetris/static/js/runtime~main.fcb3c4a7.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/tetris/static/media/bg.2810fe6b.png"
+  }
+]);
