ec1ed47e7d4f93dce31f425e0a8e8c015e7ead8e
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-17 11:02:14 +0500

    Очистка заполненных рядов

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,15 +1,30 @@
 import { useState, useEffect } from "react";
-import { createStage } from "../utils/gameHelpers";
+import { createStage, STAGE_WIDTH } from "../utils/gameHelpers";
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const rowsReducer = (acc, row) => {
+      if (row.every(cell => cell[0] !== "empty")) {
+        setRowsCleared(rows => rows + 1);
+        acc.unshift(new Array(STAGE_WIDTH).fill(["empty", "clear"]))
+      } else {
+        acc.push(row);
+      }
+      return acc;
+    }
+
+    const sweepRows = (newStage) => newStage.reduce(rowsReducer, []);
+
     const updateStage = prevStage => {
       const newStage = prevStage.map(row =>
         row.map(cell => (cell[1] === "clear" ? ["empty", "clear"] : cell))
       );
-      
+
       player.tetromino.forEach((row, y) => {
         row.forEach((value, x) => {
           if (value !== 0) {
@@ -23,6 +38,7 @@ export const useStage = (player, resetPlayer) => {
 
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -31,5 +47,5 @@ export const useStage = (player, resetPlayer) => {
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]); // ???
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
